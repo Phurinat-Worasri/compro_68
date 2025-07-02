aac3fab5cfd4d95b803f1d641badb8dbcc491b50
Phurinat-Worasri <s6706022510301@email.kmutnb.ac.th>
2025-07-02 10:24:53 +0700

    lecture

---
+++ Lecture-02/examole2.py
@@ -0,0 +1,5 @@
+celsius = input("Enter the temperature in Celsius: ")
+celsius = float(celsius)
+
+fahrenheit = (celsius * 9/5) + 32
+print("The temperature in Fahrenheit is: " + str(fahrenheit))
---
+++ Lecture-02/type_convert.py
@@ -0,0 +1,6 @@
+age = 25
+height = 5.6
+age = int(height)
+num_str = "123"
+num = int(num_str)
+print(num)
