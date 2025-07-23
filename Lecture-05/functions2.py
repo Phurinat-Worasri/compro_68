1f01b8bf84d6aadb18eb1ad4e0cd33b99a15963e
Phurinat-Worasri <s6706022510301@email.kmutnb.ac.th>
2025-07-23 11:02:29 +0700

    Lecture5

---
+++ Lecture-05/functions2.py
@@ -0,0 +1,7 @@
+def greet():
+    print('I am Phurinat')
+    print('King of the britons')
+
+print('I have a message for you.')
+greet()
+print('Goodbye!')
---
+++ Lecture-05/main.py
@@ -0,0 +1,11 @@
+import math_operarions
+
+result_add = math_operarions.add(10,5)
+result_subtract = math_operarions.subtract(10,5)
+result_multiply = math_operarions.multiply(10,5)
+result_divide = math_operarions.divide(10, 5)
+
+print(f"Addition: {result_add}")
+print(f"subtracttion: {result_subtract}")
+print(f"multiplication: {result_multiply}")
+print(f"Division: {result_divide}")
